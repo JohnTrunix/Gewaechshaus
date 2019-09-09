fb749495969ead5f095bdd8e5c1a1c365d2fa9b0
Thomas Glauser <thomi.glauser@gmail.com>
2019-09-09 20:52:54 +0200

    error message

--- oberfläche/einstellungen.php
+++ oberfläche/einstellungen.php
@@ -111,9 +111,26 @@
 
 
   <input type="submit" value="Speichern">
+
+  <div id="error_div">Fehler!</div>
+  <div id="success_div">Erfolgreich!</div>
 </form> 
 
 </div>
+
+<script type="text/javascript">
+  var url = window.location.href;
+  var error_msg = document.getElementById('error_div');
+  var success_msg = document.getElementById('success_div');
+
+  if ( url.search( 'fehler' ) > 0 ) {
+    error_msg.style.display = "flex";
+    success_msg.style.display = "none";
+  } else if ( url.search( 'erfolgreich' ) > 0 ) {
+      success_msg.style.display = "flex";
+      error_msg.style.display = "none";
+  }
+</script>
 
 </body>
 </html> 
