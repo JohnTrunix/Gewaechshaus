f0d5f74302d4a177e977be59a5246e3446ddd513
Thomas Glauser <thomi.glauser@gmail.com>
2019-09-12 19:55:37 +0200

    mv code

--- oberfläche/einstellungen.php
+++ oberfläche/einstellungen.php
@@ -1,3 +1,41 @@
+<?php
+$servername = "localhost";
+$username = "datenbank";
+$password = "rasp";
+$dbname = "datenbank";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT slot, pflanze, temperatur, lichtstunden, wassermenge, luftfeuchtigkeit FROM parameter";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $slot = $row["slot"];
+        $pflanze = $row["pflanze"];
+        $temperatur = $row["temperatur"];
+        $lichtstunden = $row["lichtstunden"];
+        $wassermenge = $row["wassermenge"];
+        $luftfeuchtigkeit = $row["luftfeuchtigkeit"];
+    }
+
+    echo $slot;
+    echo $pflanze;
+    echo $temperatur;
+    echo $lichtstunden;
+    echo $wassermenge;
+    echo $luftfeuchtigkeit;
+
+} else {
+    echo "FEHLER!!!";
+}
+$conn->close();
+?>
 <!DOCTYPE html>
 <html>
     <head>
