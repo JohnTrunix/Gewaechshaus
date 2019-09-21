f99894c05102464c0e94295fe2a73a1741a8d93e
Thomas Glauser <thomi.glauser@gmail.com>
2019-09-21 16:42:50 +0200

    livewerte direkt anzeigen

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -28,59 +28,17 @@ require 'php/parameter_download.php';
             <p class="menu_titel">Live Werte</p>
             <div class="sensorwert sensorwert_1">
                 <img src="img/idea.svg" class="sensor_icon">
-                <?php
-$link = mysqli_connect("localhost", "datenbank", "rasp", "datenbank");
-$str2 = "Lux";
-$sql = "SELECT sensorwert FROM sensor_licht_1 ORDER BY zeit DESC LIMIT 1;";
-if ($result = mysqli_query($link, $sql)) {
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result)) {
-            echo "<a class='sensor_zahl'>";
-            echo $row['sensorwert'] . " " . $str2;
-            echo "</a>";
-        }
-        mysqli_free_result($result);
-    }
-}
-?>
+                <a class='sensor_zahl'></a>
                 <a class="sensor_beschreibung">Lichtstärke</a>
             </div>
             <div class="sensorwert sensorwert_2">
                 <img src="img/breeze.svg" class="sensor_icon">
-                <?php
-$link = mysqli_connect("localhost", "datenbank", "rasp", "datenbank");
-$str2 = "%";
-$sql = "SELECT sensorwert FROM sensor_luftfeuchtigkeit_1 ORDER BY zeit DESC LIMIT 1;";
-if ($result = mysqli_query($link, $sql)) {
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result)) {
-            echo "<a class='sensor_zahl'>";
-            echo $row['sensorwert'] . " " . $str2;
-            echo "</a>";
-        }
-        mysqli_free_result($result);
-    }
-}
-?>
+                <a class='sensor_zahl'></a>
                 <a class="sensor_beschreibung">Luftfeuchtigkeit</a>
             </div>
             <div class="sensorwert sensorwert_3">
                 <img src="img/thermometer.svg" class="sensor_icon">
-                <?php
-$link = mysqli_connect("localhost", "datenbank", "rasp", "datenbank");
-$str2 = "°C";
-$sql = "SELECT sensorwert FROM sensor_temperatur_1 ORDER BY zeit DESC LIMIT 1;";
-if ($result = mysqli_query($link, $sql)) {
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result)) {
-            echo "<a class='sensor_zahl'>";
-            echo $row['sensorwert'] . " " . $str2;
-            echo "</a>";
-        }
-        mysqli_free_result($result);
-    }
-}
-?>
+                <a class='sensor_zahl'></a>
                 <a class="sensor_beschreibung">Temperatur</a>
             </div>
 		</div>
