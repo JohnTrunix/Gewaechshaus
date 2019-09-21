3138ff7f40e27e08f2c211c060f5be425ddae003
Thomas Glauser <thomi.glauser@gmail.com>
2019-09-21 20:40:36 +0200

    schedule time daily

--- usr/bin/gewaechshaus/run.py
+++ usr/bin/gewaechshaus/run.py
@@ -5,8 +5,8 @@ import schedule
 import time
 
 schedule.every(1).minutes.do(start_sensorabfrage)
-schedule.every().day.at("10:30").do(start_datenbegrenzung)
-schedule.every().day.at("10:30").do(start_zeit_update)
+schedule.every().day.at("10:00").do(start_datenbegrenzung)
+schedule.every().day.at("10:00").do(start_zeit_update)
 
 while True:
     schedule.run_pending()
