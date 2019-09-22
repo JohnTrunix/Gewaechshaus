7742e703b6fae8df2bf1a01eba81661755152389
Thomas Glauser <thomi.glauser@gmail.com>
2019-09-22 19:32:08 +0200

    bodenfeuchtigkeit

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -41,6 +41,11 @@ require 'api/betriebsmodus/betriebsmodus_download.php';
                 <img src="/img/thermometer.svg" class="sensor_icon">
                 <a class='sensor_zahl' id='temperatur_wert'></a>
                 <a class="sensor_beschreibung">Temperatur</a>
+			</div>
+			<div class="sensorwert sensorwert_4">
+                <img src="/img/wave.svg" class="sensor_icon">
+                <a class='sensor_zahl' id='bodenfeuchtigkeit_wert'></a>
+                <a class="sensor_beschreibung">Bodenfeuchtigkeit</a>
             </div>
         </div>
         <div class="mitte">
