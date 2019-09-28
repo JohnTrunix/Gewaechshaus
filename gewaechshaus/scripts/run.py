9b88ffeeb1d60793e09bfa83003b10646cf67ba6
Thomas Glauser <thomi.glauser@gmail.com>
2019-09-28 15:50:20 +0200

    logging in run.py

--- gewaechshaus/scripts/run.py
+++ gewaechshaus/scripts/run.py
@@ -3,15 +3,29 @@ from datenbegrenzung import start_datenbegrenzung
 from zeit_update import start_zeit_update
 import schedule
 import time
+import logging
+
+
+logger = logging.getLogger('Gewaechshaus | run.py')
+logger.setLevel(logging.DEBUG)
+ch = logging.FileHandler(
+    'B:/SW Repos/Gewaechshaus/gewaechshaus/log/gewaechshaus.log')
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 
 schedule.every(1).minutes.do(start_sensorabfrage)
 schedule.every().day.at("10:00").do(start_datenbegrenzung)
 schedule.every().day.at("10:00").do(start_zeit_update)
 
+
 while True:
     try:
-        print("Überprüfe auf Aufträge.")
+        logger.debug('Starte ausstehende Aufträge')
         schedule.run_pending()
-        time.sleep(10)
+        time.sleep(30)
     except:
-        print("Genereller Fehler!")
+        logger.warning('Fehler bei schedule.run_pending()')
