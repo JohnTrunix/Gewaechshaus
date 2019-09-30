f41da0be0b1fbbbfe3dd3c28924bdc529d3bcb54
Thomas Glauser <thomi.glauser@gmail.com>
2019-09-30 19:39:05 +0200

    calc seconds

--- gewaechshaus/client/js/datumrechner.js
+++ gewaechshaus/client/js/datumrechner.js
@@ -2,8 +2,42 @@ var now = new Date(),
 	minDate = now.toISOString().substring(0, 10);
 $("#programm_datum_ende").prop("min", minDate);
 
-var date1, date2;
-date1 = new Date("09 30, 2019 18:36:23");
-date2 = new Date("10 23, 2019 14:00:00");
-var seconds = Math.abs(date1 - date2) / 1000;
-document.write("<br>Difference between 2 dates: " + seconds);
+function datum_nach_sekunden(input_datumzeit1, input_datumzeit2) {
+	var input_datumzeit1, input_datumzeit2;
+
+	var input_zeit1 = input_datumzeit1.substr(-8);
+	var input_jahr1 = input_datumzeit1.substr(0, 4);
+	var input_monat1 = input_datumzeit1.substr(5, 2);
+	var input_tag1 = input_datumzeit1.substr(8, 2);
+
+	var datum1_formatiert =
+		input_monat1 +
+		" " +
+		input_tag1 +
+		"," +
+		" " +
+		input_jahr1 +
+		" " +
+		input_zeit1;
+
+	var input_zeit2 = input_datumzeit2.substr(-8);
+	var input_jahr2 = input_datumzeit2.substr(0, 4);
+	var input_monat2 = input_datumzeit2.substr(5, 2);
+	var input_tag2 = input_datumzeit2.substr(8, 2);
+
+	var datum2_formatiert =
+		input_monat2 +
+		" " +
+		input_tag2 +
+		"," +
+		" " +
+		input_jahr2 +
+		" " +
+		input_zeit2;
+
+	var date1, date2;
+	date1 = new Date(datum1_formatiert);
+	date2 = new Date(datum2_formatiert);
+	var seconds = Math.abs(date1 - date2) / 1000;
+	return seconds;
+}
