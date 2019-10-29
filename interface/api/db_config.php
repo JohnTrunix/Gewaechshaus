6f7e6db6ccccd379a866c8928ce89470892dee75
Thomas Glauser <thomi.glauser@gmail.com>
2019-10-29 18:52:35 +0100

    db_config comment

--- interface/api/db_config.php
+++ interface/api/db_config.php
@@ -1,10 +1,23 @@
 <?php
-$servername = "localhost";
-$username = "datenbank";
-$password = "rasp";
-$dbname = "datenbank";
+// Die db_config beinhaltet die Verbindungsdaten der Datenbank
+// und stellt bei ausführung eine Datenbankverbindung her.
 
+// Verbindungsdaten
+//======================================================================
+// Datenbank Adresse
+$servername = "localhost";
+// Benutzername
+$username = "datenbank";
+// Passwort
+$password = "rasp";
+// Datenbank
+$dbname = "datenbank";
+//======================================================================
+
+// Datenbankverbindung herstellen
+//======================================================================
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Datenbank connection failed: " . $conn->connect_error);
 }
+//======================================================================
