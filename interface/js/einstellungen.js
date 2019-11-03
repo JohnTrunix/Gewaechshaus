f3dd46696d444b0e9c4bf78fe1e051ed9c6365f0
Thomas Glauser <thomi.glauser@gmail.com>
2019-11-03 20:24:46 +0100

    slider to js file

--- interface/js/einstellungen.js
+++ interface/js/einstellungen.js
@@ -1,7 +1,7 @@
 // Die Datei parameter.js kommuniziert über die API mit der Datenbank und liest
 // alle relevanten Parameterdaten für die Datei einstellungen.html aus.
 
-// Sobald ein Slot gewählt wurde die Daten anfordern.
+// Sobald ein Slot gewählt wurde die Parameterdaten anfordern.
 //======================================================================
 function selected_slot(selectObject) {
 	var slot = selectObject.value;
@@ -43,4 +43,47 @@ function selected_slot(selectObject) {
 		xhr.send();
 	}
 }
+//======================================================================
+
+// Steuerung für Slider 1 [Temperatur]
+//======================================================================
+var slider_1 = document.getElementById("temperatur");
+var output_1 = document.getElementById("wert_temperatur");
+output_1.innerHTML = slider_1.value;
+
+slider_1.oninput = function() {
+	output_1.innerHTML = this.value;
+};
+//======================================================================
+
+// Steuerung für Slider 2 [Lichtstunden]
+//======================================================================
+var slider_2 = document.getElementById("lichtstunden");
+var output_2 = document.getElementById("wert_lichtstunden");
+output_2.innerHTML = slider_2.value;
+
+slider_2.oninput = function() {
+	output_2.innerHTML = this.value;
+};
+//======================================================================
+
+// Steuerung für Slider 3 [Wassermenge]
+//======================================================================
+var slider_3 = document.getElementById("wassermenge");
+var output_3 = document.getElementById("wert_wassermenge");
+output_3.innerHTML = slider_3.value;
+
+slider_3.oninput = function() {
+	output_3.innerHTML = this.value;
+};
+
+// Steuerung für Slider 4 [Luftfeuchtigkeit]
+//======================================================================
+var slider_4 = document.getElementById("luftfeuchtigkeit");
+var output_4 = document.getElementById("wert_luftfeuchtigkeit");
+output_4.innerHTML = slider_4.value;
+
+slider_4.oninput = function() {
+	output_4.innerHTML = this.value;
+};
 //======================================================================
