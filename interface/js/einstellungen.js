b9e106040f9a62f2e1ba99b069aad614a9699e15
Thomas Glauser <thomi.glauser@gmail.com>
2019-11-03 20:29:21 +0100

    js fix

--- interface/js/einstellungen.js
+++ interface/js/einstellungen.js
@@ -1,47 +1,49 @@
 // Die Datei parameter.js kommuniziert über die API mit der Datenbank und liest
 // alle relevanten Parameterdaten für die Datei einstellungen.html aus.
 
-// Sobald ein Slot gewählt wurde die Parameterdaten anfordern.
+// get_parameter liest alle relevanten Daten durch die API aus der Datenbank.
+//======================================================================
+function get_parameter() {
+	// Neuer XMLHttpRequest erstellen
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "/api/api.php?parameter_read", true);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	// Wenn Bereit --> XMLHttpRequest ausführen
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			array_parameter = JSON.parse(xhr.responseText);
+			return array_parameter;
+		}
+	};
+
+	xhr.send();
+}
+get_parameter();
+//======================================================================
+
+// Sobald ein Slot per HTML Button gewählt wurde die Parameterdaten auf die HTML Elemente übertragen.
 //======================================================================
 function selected_slot(selectObject) {
-	var slot = selectObject.value;
-	parameter_download(slot);
-
-	function parameter_download(slot) {
-		value = slot;
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", "/api/api.php?parameter_read", true);
-		xhr.setRequestHeader(
-			"Content-type",
-			"application/x-www-form-urlencoded"
-		);
-
-		xhr.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				array_parameter = JSON.parse(xhr.responseText);
-				document.getElementById("pflanze").value =
-					array_parameter[value - 1].pflanze;
-				document.getElementById("temperatur").value =
-					array_parameter[value - 1].temperatur;
-				document.getElementById("lichtstunden").value =
-					array_parameter[value - 1].lichtstunden;
-				document.getElementById("wassermenge").value =
-					array_parameter[value - 1].wassermenge;
-				document.getElementById("luftfeuchtigkeit").value =
-					array_parameter[value - 1].luftfeuchtigkeit;
-				document.getElementById("wert_temperatur").innerHTML =
-					array_parameter[value - 1].temperatur;
-				document.getElementById("wert_lichtstunden").innerHTML =
-					array_parameter[value - 1].lichtstunden;
-				document.getElementById("wert_wassermenge").innerHTML =
-					array_parameter[value - 1].wassermenge;
-				document.getElementById("wert_luftfeuchtigkeit").innerHTML =
-					array_parameter[value - 1].luftfeuchtigkeit;
-			}
-		};
-
-		xhr.send();
-	}
+	var value = selectObject.value;
+	document.getElementById("pflanze").value =
+		array_parameter[value - 1].pflanze;
+	document.getElementById("temperatur").value =
+		array_parameter[value - 1].temperatur;
+	document.getElementById("lichtstunden").value =
+		array_parameter[value - 1].lichtstunden;
+	document.getElementById("wassermenge").value =
+		array_parameter[value - 1].wassermenge;
+	document.getElementById("luftfeuchtigkeit").value =
+		array_parameter[value - 1].luftfeuchtigkeit;
+	document.getElementById("wert_temperatur").innerHTML =
+		array_parameter[value - 1].temperatur;
+	document.getElementById("wert_lichtstunden").innerHTML =
+		array_parameter[value - 1].lichtstunden;
+	document.getElementById("wert_wassermenge").innerHTML =
+		array_parameter[value - 1].wassermenge;
+	document.getElementById("wert_luftfeuchtigkeit").innerHTML =
+		array_parameter[value - 1].luftfeuchtigkeit;
 }
 //======================================================================
 
