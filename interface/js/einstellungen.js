124b5dd731ff8f0d15bf7a5dd5e339bd834aea3d
Thomas Glauser <thomi.glauser@gmail.com>
2019-11-03 20:18:01 +0100

    einstellungen get data to file

---
+++ interface/js/einstellungen.js
@@ -0,0 +1,40 @@
+function selected_slot(selectObject) {
+	var slot = selectObject.value;
+	parameter_download(slot);
+
+	function parameter_download(slot) {
+		value = slot;
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "/api/api.php?parameter_read", true);
+		xhr.setRequestHeader(
+			"Content-type",
+			"application/x-www-form-urlencoded"
+		);
+
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				array_parameter = JSON.parse(xhr.responseText);
+				document.getElementById("pflanze").value =
+					array_parameter[value - 1].pflanze;
+				document.getElementById("temperatur").value =
+					array_parameter[value - 1].temperatur;
+				document.getElementById("lichtstunden").value =
+					array_parameter[value - 1].lichtstunden;
+				document.getElementById("wassermenge").value =
+					array_parameter[value - 1].wassermenge;
+				document.getElementById("luftfeuchtigkeit").value =
+					array_parameter[value - 1].luftfeuchtigkeit;
+				document.getElementById("wert_temperatur").innerHTML =
+					array_parameter[value - 1].temperatur;
+				document.getElementById("wert_lichtstunden").innerHTML =
+					array_parameter[value - 1].lichtstunden;
+				document.getElementById("wert_wassermenge").innerHTML =
+					array_parameter[value - 1].wassermenge;
+				document.getElementById("wert_luftfeuchtigkeit").innerHTML =
+					array_parameter[value - 1].luftfeuchtigkeit;
+			}
+		};
+
+		xhr.send();
+	}
+}
