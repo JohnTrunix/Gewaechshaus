a53cb1186b3bc317c20530e64b9db68a877006be
Thomas Glauser <thomi.glauser@gmail.com>
2019-11-16 22:00:22 +0100

    message display

--- interface/js/message.js
+++ interface/js/message.js
@@ -5,14 +5,21 @@
 //======================================================================
 var url = window.location.href;
 if (url.search("fehler") > 0) {
-	display_message();
+	display_message("fehler");
+} else if (url.search("erfolgreich") > 0) {
+	display_message("erfolgreich");
 }
 //======================================================================
 
 // Steuerung der Animation
 //======================================================================
-function display_message() {
-	var x = document.getElementById("error_message");
+function display_message(status) {
+	var status, x;
+	if (status == "fehler") {
+		x = document.getElementById("error_message");
+	} else if (status == "erfolgreich") {
+		x = document.getElementById("success_message");
+	}
 	x.className = "show";
 	setTimeout(function() {
 		x.className = x.className.replace("show", "");
