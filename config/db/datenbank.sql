9ad90dbf37425631a40fec653a3c4e7a74fa24a2
Thomas Glauser <thomi.glauser@gmail.com>
2019-11-19 21:23:35 +0100

    db fix

--- config/db/datenbank.sql
+++ config/db/datenbank.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Erstellungszeit: 19. Nov 2019 um 17:27
+-- Erstellungszeit: 19. Nov 2019 um 21:21
 -- Server-Version: 10.1.38-MariaDB-0+deb9u1
--- PHP-Version: 7.0.33-0+deb9u5
+-- PHP-Version: 7.0.33-0+deb9u6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -25,25 +25,31 @@ SET time_zone = "+00:00";
 --
 -- Tabellenstruktur für Tabelle `betriebsmodus`
 --
+-- Erstellt am: 19. Nov 2019 um 20:18
+--
 
 CREATE TABLE `betriebsmodus` (
   `ID` int(11) NOT NULL,
   `parameter_slot` int(11) NOT NULL,
-  `parameter_name` text COLLATE utf8_bin NOT NULL,
   `programm_status` text COLLATE utf8_bin NOT NULL,
   `datetime` datetime NOT NULL,
   `programm_datum_ende` date NOT NULL,
-  `programm_zeit_ende` time NOT NULL,
-  `wasser_gegeben_heute` int(11) NOT NULL,
-  `wasser_gegeben_total` int(11) NOT NULL,
-  `licht_gegeben_heute` int(11) NOT NULL,
-  `licht_gegeben_total` int(11) NOT NULL
+  `programm_zeit_ende` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Daten für Tabelle `betriebsmodus`
+--
+
+INSERT INTO `betriebsmodus` (`ID`, `parameter_slot`, `programm_status`, `datetime`, `programm_datum_ende`, `programm_zeit_ende`) VALUES
+(1, 1, 'stop', '2019-11-03 20:35:06', '0000-00-00', '00:00:00');
 
 -- --------------------------------------------------------
 
 --
 -- Tabellenstruktur für Tabelle `parameter`
+--
+-- Erstellt am: 19. Nov 2019 um 20:18
 --
 
 CREATE TABLE `parameter` (
@@ -55,10 +61,28 @@ CREATE TABLE `parameter` (
   `luftfeuchtigkeit` text COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Daten für Tabelle `parameter`
+--
+
+INSERT INTO `parameter` (`slot`, `pflanze`, `temperatur`, `lichtstunden`, `wassermenge`, `luftfeuchtigkeit`) VALUES
+(1, 'Default1', '27', '4.8', '0.3', '56'),
+(2, 'Default2', '26', '5.3', '1', '56'),
+(3, 'Default3', '30', '19.1', '0.3', '70'),
+(4, 'Default4', '25', '5.9', '0.2', '55'),
+(5, 'Default5', '25', '5.2', '0.2', '59'),
+(6, 'Default6', '24', '23.6', '0.2', '63'),
+(7, 'Default7', '24', '19', '0.3', '60'),
+(8, 'Default8', '27', '6.3', '1', '61'),
+(9, 'Default9', '27', '5.2', '0.1', '54'),
+(10, 'Default10', '28', '7.8', '0.1', '68');
+
 -- --------------------------------------------------------
 
 --
 -- Tabellenstruktur für Tabelle `sensor_bodenfeuchtigkeit_1`
+--
+-- Erstellt am: 19. Nov 2019 um 20:18
 --
 
 CREATE TABLE `sensor_bodenfeuchtigkeit_1` (
@@ -71,6 +95,8 @@ CREATE TABLE `sensor_bodenfeuchtigkeit_1` (
 --
 -- Tabellenstruktur für Tabelle `sensor_licht_1`
 --
+-- Erstellt am: 19. Nov 2019 um 20:18
+--
 
 CREATE TABLE `sensor_licht_1` (
   `sensorwert` text COLLATE utf8_bin NOT NULL,
@@ -82,6 +108,8 @@ CREATE TABLE `sensor_licht_1` (
 --
 -- Tabellenstruktur für Tabelle `sensor_luftfeuchtigkeit_1`
 --
+-- Erstellt am: 19. Nov 2019 um 20:18
+--
 
 CREATE TABLE `sensor_luftfeuchtigkeit_1` (
   `sensorwert` text COLLATE utf8_bin NOT NULL,
@@ -92,6 +120,8 @@ CREATE TABLE `sensor_luftfeuchtigkeit_1` (
 
 --
 -- Tabellenstruktur für Tabelle `sensor_temperatur_1`
+--
+-- Erstellt am: 19. Nov 2019 um 20:18
 --
 
 CREATE TABLE `sensor_temperatur_1` (
