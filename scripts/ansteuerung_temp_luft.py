1988d2d14f57c72c10b4f19129d950625461e6e2
Thomas Glauser <thomi.glauser@gmail.com>
2019-11-30 20:08:37 +0100

    fehlermeldung temp luft

--- scripts/ansteuerung_temp_luft.py
+++ scripts/ansteuerung_temp_luft.py
@@ -1,11 +1,12 @@
 import datenbank_abfrage
 import sensor_abfrage
 from ansteuerung_pwm_shield import heizung_ein, heizung_aus, befeuchter_ein, befeuchter_aus, luefter_klein_ein, luefter_klein_aus, grundstellung
+from fehlermeldungen import neue_fehlermeldung
+
 
 def start_ansteuerung_temp_luft():
     try:
         if datenbank_abfrage.programm_status == 1:
-            print("Programm läuft")
             if (int(sensor_abfrage.temperatur_gerundet) < int(datenbank_abfrage.temperatur)):
                 heizung_ein()
                 status_heizung = 1
@@ -13,7 +14,6 @@ def start_ansteuerung_temp_luft():
                 heizung_aus()
                 status_heizung = 0
             else:
-                print("Keine Änderung Heizung")
                 pass
 
             if (int(sensor_abfrage.luftfeuchtigkeit_gerundet) < int(datenbank_abfrage.luftfeuchtigkeit)):
@@ -23,7 +23,6 @@ def start_ansteuerung_temp_luft():
                 befeuchter_aus()
                 status_befeuchter = 0
             else:
-                print("Keine Änderung Befeuchter")
                 pass
 
             if status_heizung == 1 or status_befeuchter == 1:
@@ -35,4 +34,5 @@ def start_ansteuerung_temp_luft():
         else:
             grundstellung()
     except:
-        print("Fehler bei start_ansteuerung_temp_luft()")
+        neue_fehlermeldung(
+            "[ansteuerung_temp_luft] Fehler bei der Zyklusberechnung der Heizung und des Befeuchters.")
