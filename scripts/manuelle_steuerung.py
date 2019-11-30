0ffcb52fd6585ce422e730a6756f998cb510adc1
Thomas Glauser <thomi.glauser@gmail.com>
2019-11-30 22:13:55 +0100

    steuerung

--- scripts/manuelle_steuerung.py
+++ scripts/manuelle_steuerung.py
@@ -1,5 +1,8 @@
 # my_example.py
 import argparse
+import os
+import time
+from ansteuerung_pwm_shield import grundstellung
 parser = argparse.ArgumentParser(description='Manuelle Ansteuerung')
 parser.add_argument(
     '--stop',
@@ -20,3 +23,20 @@ arguments = parser.parse_args()
 print(arguments.stop)
 print(arguments.start)
 print(arguments.herunterfahren)
+
+if arguments.stop == 1:
+    grundstellung()
+    time.sleep(2)
+    os.system("sudo service gewaechshaus stop")
+elif arguments.start == 1:
+    os.system("sudo service gewaechshaus stop")
+    time.sleep(2)
+    os.system("sudo service gewaechshaus start")
+elif arguments.herunterfahren == 1:
+    os.system("sudo service gewaechshaus stop")
+    time.sleep(2)
+    grundstellung()
+    time.sleep(10)
+    os.system("sudo shutdown -h now")
+else:
+    pass
