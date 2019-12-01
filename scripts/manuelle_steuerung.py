f1fe527c809d3530b3f32d66a2e4a79ee3eca0d3
Thomas Glauser <thomi.glauser@gmail.com>
2019-12-01 16:22:10 +0100

    help rm

--- scripts/manuelle_steuerung.py
+++ scripts/manuelle_steuerung.py
@@ -7,17 +7,14 @@ parser = argparse.ArgumentParser(description='Manuelle Ansteuerung')
 parser.add_argument(
     '--stop',
     default=0,
-    help='Wenn 1 -> System wird gestoppt und in Grundstellung positioniert.'
 )
 parser.add_argument(
     '--start',
     default=0,
-    help='Wenn 1 -> System wird gestartet.'
 )
 parser.add_argument(
     '--herunterfahren',
     default=0,
-    help='Wenn 1 -> System wird in gestoppt, in Grundstellung gebracht und heruntergefahren.'
 )
 arguments = parser.parse_args()
 print(arguments.stop)
