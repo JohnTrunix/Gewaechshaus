a7b3594ab4c7ef933d68277efc8f1ef1ce131006
Thomas Glauser <thomi.glauser@gmail.com>
2019-12-01 17:17:07 +0100

    manuelle steuerung betriebsmeldungen

--- scripts/manuelle_steuerung.py
+++ scripts/manuelle_steuerung.py
@@ -1,35 +1,59 @@
-# my_example.py
-import argparse
-import os
-import time
-from ansteuerung_pwm_shield import grundstellung
-parser = argparse.ArgumentParser(description='Manuelle Ansteuerung')
-parser.add_argument(
-    '--stop',
-    default=0,
-)
-parser.add_argument(
-    '--start',
-    default=0,
-)
-parser.add_argument(
-    '--herunterfahren',
-    default=0,
-)
-arguments = parser.parse_args()
-print(arguments.stop)
-print(arguments.start)
-print(arguments.herunterfahren)
+from betriebsmeldungen import neue_betriebsmeldung
+
+# Import von benötigten Modulen
+# ======================================================================
+try:
+    import argparse
+    import os
+    import time
+    from ansteuerung_pwm_shield import grundstellung
+except:
+    neue_betriebsmeldung(
+        "[manuelle_steuerung] Fehler bei der importierung von Modulen.")
+# ======================================================================
+
+
+# Definierung von Startoptionen
+# ======================================================================
+try:
+    parser = argparse.ArgumentParser(description='Manuelle Ansteuerung')
+    parser.add_argument(
+        '--stop',
+        default=0,
+        help='Wenn 1 -> System wird gestoppt und in Grundstellung positioniert.'
+    )
+    parser.add_argument(
+        '--start',
+        default=0,
+        help='Wenn 1 -> System wird gestartet.'
+    )
+    parser.add_argument(
+        '--herunterfahren',
+        default=0,
+        help='Wenn 1 -> System wird in gestoppt, in Grundstellung gebracht und heruntergefahren.'
+    )
+    arguments = parser.parse_args()
+except:
+    neue_betriebsmeldung(
+        "[manuelle_steuerung] Fehler bei der definierung von Startsignalen.")
+# ======================================================================
+
 
 if arguments.stop == 1:
+    neue_betriebsmeldung(
+        "[manuelle_steuerung] System wird gestoppt.")
     os.system("sudo service gewaechshaus stop")
     time.sleep(2)
     grundstellung()
 elif arguments.start == 1:
+    neue_betriebsmeldung(
+        "[manuelle_steuerung] System wird gestartet.")
     os.system("sudo service gewaechshaus stop")
     time.sleep(2)
     os.system("sudo service gewaechshaus start")
 elif arguments.herunterfahren == 1:
+    neue_betriebsmeldung(
+        "[manuelle_steuerung] System wird heruntergefahren.")
     os.system("sudo service gewaechshaus stop")
     time.sleep(2)
     grundstellung()
