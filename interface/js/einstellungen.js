e3453cacb3c5e19ca8203ef114e4374341deaf7e
Thomas Glauser <thomi.glauser@gmail.com>
2019-12-03 21:31:52 +0100

    einstellungen.js comments

--- interface/js/einstellungen.js
+++ interface/js/einstellungen.js
@@ -1,21 +1,22 @@
+// Parameter von Datenbank anfordern
+//======================================================================
 function get_parameter() {
-	// Neuer XMLHttpRequest erstellen
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", "/api/api.php?parameter_read", true);
 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-	// Wenn Bereit --> XMLHttpRequest ausführen
 	xhr.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			array_parameter = JSON.parse(xhr.responseText);
 			return array_parameter;
 		}
 	};
-
 	xhr.send();
 }
 get_parameter();
+//======================================================================
 
+// Parameterwerte HTML Elementen zuweisen
+//======================================================================
 function selected_slot(selectObject) {
 	var value = selectObject.value;
 	document.getElementById("pflanze").value =
@@ -37,7 +38,10 @@ function selected_slot(selectObject) {
 	document.getElementById("wert_luftfeuchtigkeit").innerHTML =
 		array_parameter[value - 1].luftfeuchtigkeit;
 }
+//======================================================================
 
+// Steuerung für Slider1
+//======================================================================
 var slider_1 = document.getElementById("temperatur");
 var output_1 = document.getElementById("wert_temperatur");
 output_1.innerHTML = slider_1.value;
@@ -45,7 +49,10 @@ output_1.innerHTML = slider_1.value;
 slider_1.oninput = function() {
 	output_1.innerHTML = this.value;
 };
+//======================================================================
 
+// Steuerung für Slider2
+//======================================================================
 var slider_2 = document.getElementById("lichtstunden");
 var output_2 = document.getElementById("wert_lichtstunden");
 output_2.innerHTML = slider_2.value;
@@ -53,7 +60,10 @@ output_2.innerHTML = slider_2.value;
 slider_2.oninput = function() {
 	output_2.innerHTML = this.value;
 };
+//======================================================================
 
+// Steuerung für Slider3
+//======================================================================
 var slider_3 = document.getElementById("wassermenge");
 var output_3 = document.getElementById("wert_wassermenge");
 output_3.innerHTML = slider_3.value;
@@ -61,7 +71,10 @@ output_3.innerHTML = slider_3.value;
 slider_3.oninput = function() {
 	output_3.innerHTML = this.value;
 };
+//======================================================================
 
+// Steuerung für Slider4
+//======================================================================
 var slider_4 = document.getElementById("luftfeuchtigkeit");
 var output_4 = document.getElementById("wert_luftfeuchtigkeit");
 output_4.innerHTML = slider_4.value;
@@ -69,7 +82,10 @@ output_4.innerHTML = slider_4.value;
 slider_4.oninput = function() {
 	output_4.innerHTML = this.value;
 };
+//======================================================================
 
+// Display Benachrichtigungen anzeigen
+//======================================================================
 var url = window.location.href;
 if (url.search("fehler") > 0) {
 	display_message("fehler");
@@ -89,3 +105,4 @@ function display_message(status) {
 		x.className = x.className.replace("show", "");
 	}, 2800);
 }
+//======================================================================
