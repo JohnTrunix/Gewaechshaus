d1cebfaa6a05fed406fa7f9c9644c45a15a0e4dd
Thomas Glauser <thomi.glauser@gmail.com>
2019-12-05 18:55:23 +0100

    format

--- interface/js/index.js
+++ interface/js/index.js
@@ -222,23 +222,15 @@ function datumzeit_jetzt() {
 	var aaaa = date.getFullYear();
 	var gg = date.getDate();
 	var mm = date.getMonth() + 1;
-
 	if (gg < 10) gg = "0" + gg;
-
 	if (mm < 10) mm = "0" + mm;
-
 	var cur_day = aaaa + "-" + mm + "-" + gg;
-
 	var hours = date.getHours();
 	var minutes = date.getMinutes();
 	var seconds = date.getSeconds();
-
 	if (hours < 10) hours = "0" + hours;
-
 	if (minutes < 10) minutes = "0" + minutes;
-
 	if (seconds < 10) seconds = "0" + seconds;
-
 	return cur_day + " " + hours + ":" + minutes + ":" + seconds;
 }
 //======================================================================
@@ -247,12 +239,10 @@ function datumzeit_jetzt() {
 //======================================================================
 function formatiere_datum(datum) {
 	var datum;
-
 	var input_zeit = datum.substr(-8);
 	var input_jahr = datum.substr(0, 4);
 	var input_monat = datum.substr(5, 2);
 	var input_tag = datum.substr(8, 2);
-
 	var datum_formatiert =
 		input_monat +
 		" " +
@@ -262,7 +252,6 @@ function formatiere_datum(datum) {
 		input_jahr +
 		" " +
 		input_zeit;
-
 	return datum_formatiert;
 }
 //======================================================================
@@ -273,12 +262,10 @@ function zeitrechner(input_datumzeit1, input_datumzeit2, teiler) {
 	formatiertes_datum1 = formatiere_datum(input_datumzeit1);
 	formatiertes_datum2 = formatiere_datum(input_datumzeit2);
 	var teiler;
-
 	var date1, date2;
 	date1 = new Date(formatiertes_datum1);
 	date2 = new Date(formatiertes_datum2);
 	var seconds = Math.abs(date1 - date2) / 1000;
-
 	var ergebnis = seconds / teiler;
 	return ergebnis;
 }
@@ -289,7 +276,6 @@ function zeitrechner(input_datumzeit1, input_datumzeit2, teiler) {
 function berechne_zeit_verbleibend(input_endzeit) {
 	var input_endzeit;
 	var zeit_jetzt = datumzeit_jetzt();
-
 	if (input_endzeit < zeit_jetzt) {
 		return 0;
 	} else if (input_endzeit > zeit_jetzt) {
@@ -305,16 +291,12 @@ function berechne_zeit_verbleibend(input_endzeit) {
 //======================================================================
 function berechne_prozent_verbleibend(input_startzeit, input_endzeit) {
 	var input_startzeit, input_endzeit;
-
 	var zeit_verbleibend = berechne_zeit_verbleibend(input_endzeit);
-
 	var zeit_gesamt = zeitrechner(input_startzeit, input_endzeit, 1);
-
 	var ein_prozent = zeit_gesamt / 100;
 	var prozent_gesamt = zeit_verbleibend / ein_prozent;
 	var prozent_resultat = 100 - prozent_gesamt;
 	var prozent_gerundet = Math.round(prozent_resultat);
-
 	if (prozent_gerundet <= 2) {
 		return 2 + "%";
 	} else {
