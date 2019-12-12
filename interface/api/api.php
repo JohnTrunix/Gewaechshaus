b7f27b346e05b35a5becc3e880c1746b60581d4c
Thomas Glauser <thomi.glauser@gmail.com>
2019-12-12 19:19:40 +0100

    get ip

--- interface/api/api.php
+++ interface/api/api.php
@@ -249,9 +249,7 @@ elseif (isset($_GET['temperatur_read'])) {
 // Lese IP Adresse
 //======================================================================
 elseif (isset($_GET['ip_adresse_read'])) {
-    $command = "/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
-    $localIP = exec($command);
-    echo $localIP;
+    echo $_SERVER['HTTP_HOST'];
 }
 //======================================================================
 
